65b5902709a0e3083b395422db71bdffd61a204f
Ayaka Hosoda <ayaka.hosoda@icloud.com>
2025-10-12 09:01:52 +0900

    style: change the file extension

---
+++ src/app/record.ts
@@ -0,0 +1,39 @@
+type DailyRecord = {
+    bedTime: Date; //寝た時刻
+    wakeUpTime: Date;//起きた時刻
+    studyTime: Date; //勉強時間
+    mediaTime: Date;
+    exercise: false;
+    reading: false;
+    breakfast: false;
+    assistance: false;
+}
+    
+type DailyGoal = {
+    bedTimeGoal: Date; //寝た時刻
+    wakeUpTimeGoal: Date;//起きた時刻
+    studyTimeGoal: Date;
+    mediaTimeGoal: Date;
+    excerciseGoal: Date;
+    readingGoal: Date;
+}
+
+type WeeklyGoal = {
+    weeklyBedTimeGoal: Date; //寝た時刻
+    weeklyWakeUpTimeGoal: Date;//起きた時刻
+    weeklyStudyTimeGoal: Date;
+    weeklyMediaTimeGoal: Date;
+    weeklyExcerciseGoal: Date;
+    weeklyReadingGoal: Date;
+}
+
+type StudentComment = {
+    comment: string;
+}
+
+type StudentAccount = {
+    id: string; //ユーザーID
+    name:string;
+    password:string;
+    class: number;
+}
