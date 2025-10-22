4912250d1483e06bc2edcd596638ea96e754a751
moshi18 <b1023119@fun.ac.jp>
2025-10-22 22:46:25 +0900

    目標入力画面への遷移作成

---
+++ src/app/goal.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import React, { useState } from "react";
+
+import Link from "next/link";
+
+function First() {
+  return (
+    <div>
+      <h1>First</h1>
+      <Link href={"/second"}>Second へ</Link>
+    </div>
+  );
+}
+
+export default First;
